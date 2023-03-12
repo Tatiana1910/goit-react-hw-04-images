225d72230024d27aafaab7d5e158def51bd5a87e
Tatiana Kramarenko <tanya19101996@gmail.com>
2023-03-12 19:25:38 +0200

    correct-modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,12 +4,15 @@ import PropTypes from 'prop-types';
 import { OverlayStyled, ModalStyled } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
+let scrollPosition = 0;
 
 export const Modal = ({ onClose, children }) => {
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
+    disableScroll();
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
+      enableScroll();
     };
   });
 
@@ -25,6 +28,25 @@ export const Modal = ({ onClose, children }) => {
     }
   };
 
+  const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({ top: scrollPosition });
+    document.documentElement.style.scrollBehavior = '';
+  };
+
+  const disableScroll = () => {
+    scrollPosition = window.scrollY;
+    document.body.style.cssText = `
+        position: fixed;
+        top: -${scrollPosition}px;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+        padding-right: ${window.innerWidth - document.body.offsetWidth}px
+      `;
+    document.documentElement.style.scrollBehavior = 'unset';
+  };
+
   return createPortal(
     <OverlayStyled onClick={handleBackdropClick}>
       <ModalStyled>{children}</ModalStyled>
@@ -34,6 +56,5 @@ export const Modal = ({ onClose, children }) => {
 };
 
 Modal.propTypes = {
-  onClick: PropTypes.func,
   onClose: PropTypes.func,
 };
